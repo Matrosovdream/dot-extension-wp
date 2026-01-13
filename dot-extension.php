40f02f2d9a2458e7b7d21bff9a373c5af911014d
Stan Matrosov <matrosovdream@gmail.com>
2026-01-13 22:21:30 +0700

    Root file, some cleanup

--- dot-extension.php
+++ dot-extension.php
@@ -15,3 +15,53 @@ define('DOTFILER_BASE_URL', __DIR__);
 require_once 'classes/init.class.php';
 
 
+add_action('init', 'init44');
+function init44() {
+
+    if( isset($_GET['migrate']) ) {
+
+        global $wpdb;
+
+        $table = $wpdb->prefix . 'frm_items_archive';
+        $query = "SELECT COUNT(*) FROM $table";
+        $results = $wpdb->get_results($query);
+
+        print_r($results);
+
+        die();
+
+    }
+
+    if( $_GET['linkss'] ) {
+
+        //
+        global $wpdb;
+        $table = $wpdb->prefix . 'frm_shortlinks';
+        $query = "SELECT COUNT(*) FROM $table";
+        $results = $wpdb->get_results($query);
+
+        // Delete all
+        $query = "DELETE FROM $table";
+        $results = $wpdb->query($query);
+
+        print_r($results);
+        die();
+
+    }
+
+    if( isset($_GET['phone']) ) {
+    
+        $data = PhoneChecker_helper::validate_phone( $phone = '14158586273' );
+        //print_r($data);
+
+    }
+
+    if( isset($_GET['linkk']) ) {
+
+        echo do_shortcode('[frm-short-link id=27315 page_id=2126]');
+        die();
+
+    }    
+
+}
+
